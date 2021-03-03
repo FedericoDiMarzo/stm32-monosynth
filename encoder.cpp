6f72787453dc0dbf912fe1c6858e500483710e33
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-03-03 15:17:50 +0100

    Encoder
    Encoder class defined

---
+++ encoder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mailf on 03/03/2021.
+//
+
+#include "encoder.h"
---
+++ encoder.h
@@ -0,0 +1,14 @@
+
+
+#ifndef STM32_MONOSYNTH_ENCODER_H
+#define STM32_MONOSYNTH_ENCODER_H
+
+#include "miosix.h"
+
+class Encoder {
+public:
+    Encoder()
+private:
+};
+
+#endif //STM32_MONOSYNTH_ENCODER_H
