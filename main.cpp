aea7c4ad07d974c33c9f13b95e9b5e2d61e680f7
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-03-08 23:44:55 +0100

    Update main.cpp
    testing Encoder

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "miosix.h"
 #include "audio.h"
 #include "audio_processor.h"
+#include "encoder.h"
 #include <functional>
 #include <math.h>
 
@@ -10,17 +11,19 @@ using namespace std;
 
 // testing an implementation of an AudioProcessor
 #define BUFFSIZE 256
+
 class AudioProcessorTest : public AudioProcessor {
 public:
     AudioProcessorTest() {
-        for(int i=0; i < BUFFSIZE; ++i) {
+        for (int i = 0; i < BUFFSIZE; ++i) {
             sinTable[i] = 4095 * ((sin(2 * 3.14 * i / BUFFSIZE) + 1) / 2);
         }
 
     }
+
     inline void process() override {
-        auto* b = getBuffer();
-        for (int i=0; i < getBufferSize(); ++i) {
+        auto *b = getBuffer();
+        for (int i = 0; i < getBufferSize(); ++i) {
             b[i] = sinTable[i];
         }
     }
@@ -38,9 +41,14 @@ int main() {
     // setting the audio processor
     audioDriver.setAudioProcessable(audioProcessorTest);
 
+    // setting the encoder
+//    Encoder encoder1;
+    Encoder::testStaticRotary();
+
     // infinite loop
     for (;;) {
-
+//        encoder1.getValue();
+        sleep(0.1);
     }
 
 }
