37af61865dc51c28b19c383c28d98d1e03d9e60a
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-03-08 23:44:17 +0100

    Encoder Class
    Tested only on TIM4

--- encoder.h
+++ encoder.h
@@ -5,10 +5,35 @@
 
 #include "miosix.h"
 
+
 class Encoder {
 public:
-    Encoder()
+
+    Encoder(GPIO_TypeDef *gpio = GPIOB, unsigned int pin1 = 4, unsigned int pin2 = 5,
+            TIM_TypeDef *timer = TIM3);
+
+    static void testStaticRotary();
+
+    /**
+     * Getter for value attribute.
+     *
+     * @return  value
+     */
+    float getValue();
+
 private:
+    TIM_TypeDef *_timer;
+
+    /**
+     * Value of the encoder, between 0 and 1.
+     */
+    float value;
+
+    /**
+     * Value from the last getValue() call.
+     */
+    float lastValue;
+
 };
 
 #endif //STM32_MONOSYNTH_ENCODER_H
