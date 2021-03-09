a86fface767e859e710d2a5ed3ff16e640ef8328
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-03-09 12:03:45 +0100

    Update main.cpp
    Testing Encoder

--- main.cpp
+++ main.cpp
@@ -3,51 +3,55 @@
 #include "audio.h"
 #include "audio_processor.h"
 #include "encoder.h"
+#include "audio_buffer.h"
 #include <functional>
 #include <math.h>
+#include <cstdint>
+
 
 using namespace miosix;
 using namespace std;
 
 // testing an implementation of an AudioProcessor
-#define BUFFSIZE 256
-
 class AudioProcessorTest : public AudioProcessor {
 public:
     AudioProcessorTest() {
-        for (int i = 0; i < BUFFSIZE; ++i) {
-            sinTable[i] = 4095 * ((sin(2 * 3.14 * i / BUFFSIZE) + 1) / 2);
+        for (int i = 0; i < AUDIO_DRIVER_BUFFER_SIZE; ++i) {
+            sinTable[i] = sin(2 * 3.14 * i / AUDIO_DRIVER_BUFFER_SIZE);
         }
 
     }
 
-    inline void process() override {
-        auto *b = getBuffer();
-        for (int i = 0; i < getBufferSize(); ++i) {
-            b[i] = sinTable[i];
+
+    void process() override {
+        auto &buffer = getBuffer();
+        auto leftChannel = buffer.getWritePointer(0);
+        for (unsigned int i = 0; i < getBufferSize(); ++i) {
+            leftChannel[i] = sinTable[i];
         }
     }
 
-    unsigned int sinTable[BUFFSIZE];
+    float sinTable[AUDIO_DRIVER_BUFFER_SIZE];
 };
 
 int main() {
     AudioProcessorTest audioProcessorTest;
+    audioProcessorTest.process();
 
     // initializing the audio driver
     AudioDriver &audioDriver = AudioDriver::getInstance();
+    audioDriver.getBuffer();
+    audioDriver.setAudioProcessable(audioProcessorTest);
     audioDriver.init(SampleRate::_44100Hz);
 
-    // setting the audio processor
-    audioDriver.setAudioProcessable(audioProcessorTest);
-
-    // setting the encoder
-//    Encoder encoder1;
-    Encoder::testStaticRotary();
+    // setting the encoder1
+    Encoder encoder1(TIM4, GPIOD, 12, 13);
+    Encoder encoder2(TIM8, GPIOC, 6, 7);
 
     // infinite loop
     for (;;) {
-//        encoder1.getValue();
+        encoder1.getValue();
+        encoder2.getValue();
         sleep(0.1);
     }
 
