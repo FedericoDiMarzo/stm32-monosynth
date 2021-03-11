a00038bc55b0cba170b52e6348e9f3dccd8fc68e
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-03-11 14:16:35 +0100

    Update virtual_analog_oscillator.cpp/h
    added parameter for frequency and DPW algorithm

--- audio_modules/virtual_analog_oscillator.cpp
+++ audio_modules/virtual_analog_oscillator.cpp
@@ -1,22 +1,42 @@
 
+#include "virtual_analog_oscillator.h"
 #include "../audio/audio.h"
 #include "../audio/audio_buffer.h"
 #include "../audio/audio_module.h"
-#include "virtual_analog_oscillator.h"
+#include "../audio/audio_processor.h"
 
-VirtualAnalogOscillator::VirtualAnalogOscillator(float sampleRate)
+VirtualAnalogOscillator::VirtualAnalogOscillator(AudioProcessor &audioProcessor)
         :
-        sampleRate(sampleRate),
+        AudioModule<1>(audioProcessor),
         frequency(2000.0),
-        phase(0),
-        lastSample(0.0) {
+        phase(0.0),
+        lastParabolicSample(0.0) {
 
 }
 
-AudioBuffer<float, 1, AUDIO_DRIVER_BUFFER_SIZE> &VirtualAnalogOscillator::process() {
-    for (uint32_t i; i < buffer.getBufferLength(); i++) {
+void VirtualAnalogOscillator::process(AudioBuffer<float, 1, AUDIO_DRIVER_BUFFER_SIZE> &buffer) {
+    // Saw DPW algorithm
+    float *p = buffer.getWritePointer(0);
+    float sampleRate = getSampleRate();
+    float currentParabolicSample = 0;
+    for (uint32_t i = 0; i < buffer.getBufferLength(); i++) {
+        p[i] = 2 * phase - 1;
+        p[i] = p[i] * p[i];
+        currentParabolicSample = p[i];
+        p[i] = p[i] - lastParabolicSample;
+        lastParabolicSample = currentParabolicSample;
+        p[i] *= sampleRate / (4 * frequency.getValue());
+        phase += frequency.getValue() / sampleRate;
+        phase = (phase > 1)? 0 : phase;
+    }
+}
 
+void VirtualAnalogOscillator::setFrequency(float f) {
+    // TODO: linear interpolation between old and new value
+    if (f < 0) {
+        // TODO: error handling
+        return;
     }
 
-    return buffer;
+    frequency.setValue(f);
 }
--- audio_modules/virtual_analog_oscillator.h
+++ audio_modules/virtual_analog_oscillator.h
@@ -5,28 +5,42 @@
 #include "../audio/audio.h"
 #include "../audio/audio_buffer.h"
 #include "../audio/audio_module.h"
-#include <array>
+#include "../audio/audio_processor.h"
+#include "../audio/audio_parameter.h"
 
-// MIT CMJ302 02Valimaki 19-31 implementation
-
+/**
+ * AudioModule implementing a virtual analog oscillator.
+ * The implementation is based on MIT CMJ302 02Valimaki 19-31
+ */
 class VirtualAnalogOscillator : public AudioModule<1> {
 public:
-    VirtualAnalogOscillator(float sampleRate);
 
-    AudioBuffer<float, 1, AUDIO_DRIVER_BUFFER_SIZE> &process() override;
+    VirtualAnalogOscillator(AudioProcessor& audioProcessor);
 
+    /**
+     * Sets the frequency of the oscillator.
+     *
+     * @param f frequency in Hz
+     */
+    void setFrequency(float f);
+
+    void process(AudioBuffer<float, 1, AUDIO_DRIVER_BUFFER_SIZE> &buffer) override;
 private:
-    float sampleRate;
 
-    float frequency;
+    /**
+     * AudioParameter for the frequency in Hz.
+     */
+    AudioParameter<float> frequency;
 
-    int phase;
+    /**
+     * Phase of the oscillator, between 0 and 1.
+     */
+    float phase;
 
-    float lastSample;
-
-    AudioBuffer<float, 1, AUDIO_DRIVER_BUFFER_SIZE> buffer;
-
-    void process(AudioBuffer<float, 1, AUDIO_DRIVER_BUFFER_SIZE> &buffer) override {};
+    /**
+     * Buffer used by the saw DPW algorithm.
+     */
+    float lastParabolicSample;
 };
 
 #endif //STM32_MONOSYNTH_VIRTUAL_ANALOG_OSCILLATOR_H
