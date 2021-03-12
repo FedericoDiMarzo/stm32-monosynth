4b1aae65cb7da81c24725393b1e4d3844bfc59b3
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-03-12 16:37:50 +0100

    Update main.cpp
    Testing a 4 encoder configuration

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include "drivers/encoder.h"
 #include "audio/audio_buffer.hpp"
 #include "audio_processors/mono_synth.h"
-//#include "drivers/SSD1306.h"
+#include "drivers/SSD1306.h"
 #include "audio/cs43l22dac.h"
 #include "audio/audio_math.h"
 #include "audio/midi.h"
@@ -22,27 +22,40 @@ int main() {
     AudioDriver &audioDriver = AudioDriver::getInstance();
     audioDriver.getBuffer();
     audioDriver.init(SampleRate::_44100Hz);
-    Cs43l22dac::setVolume(-22);
-//    MonoSynth monoSynth;
-//    audioDriver.setAudioProcessable(monoSynth);
-//
-//    // midi test
-//    uint8_t *p = noteTest;
-//    Midi::Parser parser;
-//    parser.maskChannel(2);
-//    while (p != std::end(noteTest)) {
-//        parser.parse(p);
-//    }
+    Cs43l22dac::setVolume(-20);
+    MonoSynth monoSynth;
+    audioDriver.setAudioProcessable(monoSynth);
+
+    // midi test
+    uint8_t *p = noteTest;
+    Midi::Parser parser;
+    parser.maskChannel(2);
+    while (p != std::end(noteTest)) {
+        parser.parse(p);
+    }
 
     // encoder
-//    Encoder encoder1(TIM4, GPIOD, 12, 13);
+    Encoder encoder1(TIM2, GPIOA, 0, 1);
+    Encoder encoder2(TIM1, GPIOE, 9, 11);
+    Encoder encoder3(TIM3, GPIOB, 4, 5);
+    Encoder encoder4(TIM4, GPIOD, 12, 13);
 
     // display test
 //    Ssd1306::init();
 
     // main loop
-    for (;;) {
+    volatile float value1 = 0;
+    volatile float value2 = 0;
+    volatile float value3 = 0;
+    volatile float value4 = 0;
+    while (true) {
+        value1 = encoder1.getValue();
+        value2 = encoder2.getValue();
+        value3 = encoder3.getValue();
+        value4 = encoder4.getValue();
+        sleep(0.2);
 
+        monoSynth.setFrequency(AudioMath::linearMap(value1, 0, 1, 60, 1200));
     }
 
 }
