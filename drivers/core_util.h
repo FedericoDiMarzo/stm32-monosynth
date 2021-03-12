f70362d42a10d56c32e1736ea80761470746073b
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-03-12 14:10:36 +0100

    Update core_util.h

--- drivers/core_util.h
+++ drivers/core_util.h
@@ -1,17 +1,70 @@
 
 #ifndef STM32_MONOSYNTH_CORE_UTIL_H
 #define STM32_MONOSYNTH_CORE_UTIL_H
+
 #include "miosix.h"
 
+/**
+ * Used to as an invalid result for an
+ * alternate function getter function.
+ */
 #define INVALID_AF 0xf
-#define CLOCK_FREQ 168000000
 
+/**
+ * This module defines hardware specific functions for the
+ * board stm32f407vg discovery.
+ *
+ * To provide compatibility to other boards, provide a custom
+ * implementation of the cpp file.
+ */
 namespace CoreUtil {
-    void rccEnableGpio(GPIO_TypeDef *gpio);
+    /**
+     * Enables the clock for a GPIO.
+     *
+     * @param gpio GPIO_TypeDef pointer
+     */
+    void rccEnableGpio(const GPIO_TypeDef *gpio);
 
-    void rccEnableTimer(TIM_TypeDef *timer);
+    /**
+     * Enables the clock for a timer.
+     *
+     * @param timer TIM_TypeDef pointer
+     */
+    void rccEnableTimer(const TIM_TypeDef *timer);
 
-    uint32_t gpioGetTimerAF(GPIO_TypeDef *gpio, uint32_t pin);
+    /**
+     * Enable the clock for a i2c unit.
+     *
+     * @param i2c I2C_TypeDef pointer
+     */
+    void rccEnableI2c(const I2C_TypeDef *i2c);
+
+    /**
+     * Gets the timer alternate function for a certain pin, just for
+     * pins having a TIMxCH1/CH2 routing.
+     *
+     * @param gpio target GPIO
+     * @param pin target PIN
+     * @return alternate function index
+     */
+    uint8_t getGpioTimerAf(const GPIO_TypeDef *gpio, uint8_t pin);
+
+    /**
+     * Gets the I2C alternate function for a certain pin, just for
+     * pins having a SDA/SCL routing.
+     *
+     * @param gpio target GPIO
+     * @param pin target PIN
+     * @return alternate function index
+     */
+    uint8_t getGpioI2cAf(const GPIO_TypeDef *gpio, uint8_t pin);
+
+    /**
+     * This function implements an infinite loop.
+     * It is used to handle errors, in order to
+     * establish a defined execution state.
+     */
+    void errorHandler();
 }
 
 #endif //STM32_MONOSYNTH_CORE_UTIL_H
