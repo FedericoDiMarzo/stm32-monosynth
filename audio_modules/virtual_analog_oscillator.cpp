f910eabf08cae4d73ee8a42ebb9f586f36bf14da
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-03-13 19:28:55 +0100

    Update virtual_analog_oscillator.cpp/h
    Sine algorithm

--- audio_modules/virtual_analog_oscillator.cpp
+++ audio_modules/virtual_analog_oscillator.cpp
@@ -4,39 +4,80 @@
 #include "../audio/audio_buffer.h"
 #include "../audio/audio_module.h"
 #include "../audio/audio_processor.h"
+#include "../audio/audio_math.h"
+#include "lookup_tables.h"
 
 VirtualAnalogOscillator::VirtualAnalogOscillator(AudioProcessor &audioProcessor)
         :
         AudioModule<1>(audioProcessor),
         frequency(2000.0),
+        waveType(VirtualAnalogOscillatorWaveType::SAW_DPW),
         phase(0.0),
-        lastParabolicSample(0.0) {
-
-}
+        lastParabolicSample(0.0) {}
 
 void VirtualAnalogOscillator::process(AudioBuffer<float, 1, AUDIO_DRIVER_BUFFER_SIZE> &buffer) {
-    // Saw DPW algorithm
-    float *p = buffer.getWritePointer(0);
-    float sampleRate = getSampleRate();
-    float currentParabolicSample = 0;
-    for (uint32_t i = 0; i < buffer.getBufferLength(); i++) {
-        p[i] = 2 * phase - 1;
-        p[i] = p[i] * p[i];
-        currentParabolicSample = p[i];
-        p[i] = p[i] - lastParabolicSample;
-        lastParabolicSample = currentParabolicSample;
-        p[i] *= sampleRate / (4 * frequency.getValue());
-        phase += frequency.getValue() / sampleRate;
-        phase = (phase > 1)? 0 : phase;
+    if (waveType == VirtualAnalogOscillatorWaveType::SAW_DPW) {
+        // Saw DPW algorithm
+        float *p = buffer.getWritePointer(0);
+        float sampleRate = getSampleRate();
+        float currentParabolicSample = 0;
+        float interpolatedFrequency;
+        for (uint32_t i = 0; i < buffer.getBufferLength(); i++) {
+            // frequency update
+            interpolatedFrequency = frequency.getInterpolatedValue();
+            frequency.updateSampleCount(1);
+
+            // mapping the wrapped phase between -1 and 1
+            p[i] = 2 * phase - 1;
+
+            // parabola from the phase modulo
+            p[i] = p[i] * p[i];
+
+            // storing the parabola sample in a tmp buffer
+            currentParabolicSample = p[i];
+
+            // differentiation
+            p[i] = p[i] - lastParabolicSample;
+
+            // buffering the current parabola sample
+            lastParabolicSample = currentParabolicSample;
+
+            // gain compensation
+            p[i] *= sampleRate / (4 * interpolatedFrequency);
+
+            // updating the phase
+            phase += interpolatedFrequency / sampleRate;
+
+            // phase wrapping
+            phase = (phase > 1) ? 0 : phase;
+        }
     }
+
+    else if (waveType == VirtualAnalogOscillatorWaveType::SINE)
+        processSine(buffer);
+
 }
 
-void VirtualAnalogOscillator::setFrequency(float f) {
-    // TODO: linear interpolation between old and new value
-    if (f < 0) {
-        // TODO: error handling
-        return;
-    }
 
-    frequency.setValue(f);
+void VirtualAnalogOscillator::processSawDpw(AudioBuffer<float, 1, AUDIO_DRIVER_BUFFER_SIZE> &buffer) {
+
+}
+
+void VirtualAnalogOscillator::processSine(AudioBuffer<float, 1, AUDIO_DRIVER_BUFFER_SIZE> &buffer) {
+    float *p = buffer.getWritePointer(0);
+    float interpolatedFrequency;
+    for (uint32_t i = 0; i < buffer.getBufferLength(); i++) {
+        // frequency update
+        interpolatedFrequency = frequency.getInterpolatedValue();
+        frequency.updateSampleCount(1);
+
+        // LUT sine
+        p[i] = AudioMath::sineLut(2 * M_PI * phase);
+
+        // updating the phase
+        phase += interpolatedFrequency / getSampleRate();
+
+        // phase wrapping
+        phase = (phase > 1) ? 0 : phase;
+    }
 }
--- audio_modules/virtual_analog_oscillator.h
+++ audio_modules/virtual_analog_oscillator.h
@@ -7,30 +7,54 @@
 #include "../audio/audio_module.h"
 #include "../audio/audio_processor.h"
 #include "../audio/audio_parameter.h"
+#include "../audio/midi.h"
 
 /**
  * AudioModule implementing a virtual analog oscillator.
  * The implementation is based on MIT CMJ302 02Valimaki 19-31
  */
+
+
+enum class VirtualAnalogOscillatorWaveType {
+    SAW_DPW,
+    SINE
+};
+
 class VirtualAnalogOscillator : public AudioModule<1> {
 public:
 
-    VirtualAnalogOscillator(AudioProcessor& audioProcessor);
+    VirtualAnalogOscillator(AudioProcessor &audioProcessor);
 
     /**
      * Sets the frequency of the oscillator.
      *
      * @param f frequency in Hz
      */
-    void setFrequency(float f);
+    inline void setFrequency(float f) {
+        if (f < 0) return;
+        frequency.setValue(f);
+    }
+
+    /**
+     * Sets the glide time in seconds.
+     *
+     * @param time in seconds
+     * @param sampleRate sample frequency
+     */
+    void setGlide(float time, float sampleRate) {
+        frequency.setTransitionTime(time, sampleRate);
+    }
 
     void process(AudioBuffer<float, 1, AUDIO_DRIVER_BUFFER_SIZE> &buffer) override;
+
 private:
 
     /**
      * AudioParameter for the frequency in Hz.
      */
     AudioParameter<float> frequency;
+
+    VirtualAnalogOscillatorWaveType waveType;
 
     /**
      * Phase of the oscillator, between 0 and 1.
@@ -41,6 +65,13 @@ private:
      * Buffer used by the saw DPW algorithm.
      */
     float lastParabolicSample;
+
+    void processSawDpw(AudioBuffer<float, 1, AUDIO_DRIVER_BUFFER_SIZE> &buffer);
+
+    void processSine(AudioBuffer<float, 1, AUDIO_DRIVER_BUFFER_SIZE> &buffer);
+
+
 };
+
 
 #endif //STM32_MONOSYNTH_VIRTUAL_ANALOG_OSCILLATOR_H
