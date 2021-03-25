5fdbb32630ccaff12fbb8b8b604cb67a590bc06c
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-03-25 15:48:59 +0100

    Update main.cpp
    debbugging the encoders

--- main.cpp
+++ main.cpp
@@ -36,22 +36,25 @@ void encoderThread() {
     encoder2.setSensitivity(sensitivity);
     encoder3.setSensitivity(sensitivity);
     encoder4.setSensitivity(sensitivity);
-    volatile float noteChangeTime = 1000.0;
+    volatile float noteChangeTime = 1;
     volatile float glideTime = 0.005;
-//    float frequencies[] = {50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 650, 700, 750, 800};
+    volatile float encoderValue;
     encoder1.setValue(0.5);
 
     // encoder loop
     while (true) {
         for (auto n : midiNotesValuesInAEolian) {
-            noteChangeTime = AudioMath::linearMap(encoder1.getValue(), 0, 1, 600, 20);
-            glideTime = AudioMath::linearMap(encoder2.getValue(), 0, 1, 0.005, 0.5);
             {
+//                encoderValue = encoder1.getValue();
+//                encoderValue = encoder2.getValue();
+//                encoderValue = encoder3.getValue();
+                encoderValue = encoder4.getValue();
+                noteChangeTime = AudioMath::linearMap(encoder1.getValue(), 0, 1, 600, 20);
+                glideTime = AudioMath::linearMap(encoder2.getValue(), 0, 1, 0.005, 0.5);
                 miosix::FastMutex mutex;
                 monoSynth.setNote(n);
-                if (button1.isPressed()) {
-                    monoSynth.setGlide(glideTime);
-                }
+                monoSynth.setGlide(glideTime);
+
             }
             miosix::Thread::sleep(noteChangeTime);
         }
@@ -66,7 +69,6 @@ void buttonThread() {
     // button loop
     while (true) {
         button1.update();
-        button1State = button1.isPressed();
         miosix::Thread::sleep(0.02);
     }
 }
