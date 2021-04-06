3fad832e77fce744ab8a4f65daa2d20411867548
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-04-06 19:29:38 +0200

    Update lookup_tables.h
    rampLut

--- audio_modules/lookup_tables.h
+++ audio_modules/lookup_tables.h
@@ -8,9 +8,22 @@
 
 #define LUT_TABLE_RESOLUTION 256
 
+/**
+ * This header defines various specific lookup tables.
+ */
 namespace AudioMath {
+    /**
+     * Sine function.
+     */
     LookupTable<LUT_TABLE_RESOLUTION> sineLut([](float x) { return std::sin(x); },
                                               0.0f, 2.0f * M_PI,
+                                              LookupTableEdges::PERIODIC);
+
+    /**
+     * Ramp function, periodic between 0 and 1.
+     */
+    LookupTable<LUT_TABLE_RESOLUTION> rampLut([](float x) {return x;},
+                                              0.0f, 1.0f,
                                               LookupTableEdges::PERIODIC);
 }
 
