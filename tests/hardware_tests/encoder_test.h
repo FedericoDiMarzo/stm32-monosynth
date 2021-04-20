8dc1b1056332723f2fb118548f69ae1b7020abca
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-04-20 15:00:32 +0200

    Update encoder_test.h
    adding one more encoder

--- tests/hardware_tests/encoder_test.h
+++ tests/hardware_tests/encoder_test.h
@@ -14,32 +14,37 @@ void encoderTest() {
     Encoder encoder2(Encoder2Timer, Encoder2Port, Encoder2PinA, Encoder2PinB);
     Encoder encoder3(Encoder3Timer, Encoder3Port, Encoder3PinA, Encoder3PinB);
     Encoder encoder4(Encoder4Timer, Encoder4Port, Encoder4PinA, Encoder4PinB);
+    Encoder encoder5(Encoder5Timer, Encoder5Port, Encoder5PinA, Encoder5PinB);
 
     encoder1.setSensitivity(EncoderSensitivity);
     encoder2.setSensitivity(EncoderSensitivity);
     encoder3.setSensitivity(EncoderSensitivity);
-    encoder4.setSensitivity(EncoderSensitivity);
+    encoder5.setSensitivity(EncoderSensitivity);
 
     volatile float value1;
     volatile float value2;
     volatile float value3;
     volatile float value4;
+    volatile float value5;
 
-    encoder1.setValue(0.5);
-    encoder2.setValue(0.5);
-    encoder3.setValue(0.5);
-    encoder4.setValue(0.5);
+    encoder1.setValue(0.5f);
+    encoder2.setValue(0.5f);
+    encoder3.setValue(0.5f);
+    encoder4.setValue(0.5f);
+    encoder5.setValue(0.5f);
 
     while (true) {
         encoder1.update();
         encoder2.update();
         encoder3.update();
         encoder4.update();
+        encoder5.update();
 
         value1 = encoder1.getValue();
         value2 = encoder2.getValue();
         value3 = encoder3.getValue();
         value4 = encoder4.getValue();
+        value5 = encoder5.getValue();
 
         miosix::Thread::sleep(20);
     }
