335d7353f9b7ecd065bc17334162438c70357b17
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-04-20 14:59:59 +0200

    Update main.cpp
    modifying hardware interface

--- main.cpp
+++ main.cpp
@@ -1,32 +1,19 @@
-
 #include "miosix.h"
 #include "../drivers/common/audio.h"
 #include "audio/audio_processor.h"
-#include "drivers/encoder.h"
 #include "drivers/double_encoder.h"
-#include "audio/audio_buffer.h"
 #include "audio_processors/mono_synth.h"
 #include "audio_modules/envelope.h"
 #include "audio_modules/lowpass_filter_1p.h"
-#include "drivers/ssd1306.h"
-#include "drivers/button.h"
-#include "drivers/stm32f407vg_discovery/cs43l22dac.h"
-#include "audio/audio_math.h"
 #include "midi/midi.h"
 #include "tests/midi_test_data.h"
 #include "tests/hardware_tests/encoder_test.h"
 #include "tests/hardware_tests/double_encoder_test.h"
 #include "tests/hardware_tests/button_test.h"
 #include "drivers/hardware_configuration.h"
-#include <functional>
-#include <vector>
-#include <algorithm>
-#include <cstdint>
 #include <array>
 #include <cmath>
 #include <thread>
-#include <memory>
-#include <vector>
 
 ///////// TESTING //////////
 ///////////////////////////
@@ -41,7 +28,7 @@
  */
 //#define TEST_BUTTONS
 //#define TEST_ENCODERS
-//#define TEST_DOUBLE_ENCODERS
+#define TEST_DOUBLE_ENCODERS
 ///////////////////////////
 ///////////////////////////
 
@@ -53,10 +40,13 @@ AudioDriver audioDriver(SampleRate::_44100Hz);
 // The synth engine
 MonoSynth monoSynth(audioDriver);
 
+// to change the speed of the sequencer
+volatile float noteChangeTime;
+
 // double encoders
 DoubleEncoder doubleEncoder1(DoubleEncoder1Timer, DoubleEncoder1PortEnc,
                              DoubleEncoder1PinAEnc, DoubleEncoder1PinBEnc,
-                             DoubleEncoder1PortBut, DoubleEncoder1PinBut);
+                             DoubleEncoder1PortBut, DoubleEncoder1PinBut); // TODO: verify why this encoder doesn't work
 DoubleEncoder doubleEncoder2(DoubleEncoder2Timer, DoubleEncoder2PortEnc,
                              DoubleEncoder2PinAEnc, DoubleEncoder2PinBEnc,
                              DoubleEncoder2PortBut, DoubleEncoder2PinBut);
@@ -66,6 +56,9 @@ DoubleEncoder doubleEncoder3(DoubleEncoder3Timer, DoubleEncoder3PortEnc,
 DoubleEncoder doubleEncoder4(DoubleEncoder4Timer, DoubleEncoder4PortEnc,
                              DoubleEncoder4PinAEnc, DoubleEncoder4PinBEnc,
                              DoubleEncoder4PortBut, DoubleEncoder4PinBut);
+DoubleEncoder doubleEncoder5(DoubleEncoder5Timer, DoubleEncoder5PortEnc,
+                             DoubleEncoder5PinAEnc, DoubleEncoder5PinBEnc,
+                             DoubleEncoder5PortBut, DoubleEncoder5PinBut);
 
 std::vector <Midi::MidiMessage> midiMessagesFromNoteValues(uint8_t *noteArray, size_t size) {
     std::vector <Midi::MidiMessage> midiQueue;
@@ -80,57 +73,78 @@ std::vector <Midi::MidiMessage> midiMessagesFromNoteValues(uint8_t *noteArray, s
  * Thread function for handling the hardware inputs.
  */
 void hardwareInterfaceThreadFunc() {
-    std::array < DoubleEncoder * , 4 > encoders =
-                                           {&doubleEncoder1, &doubleEncoder2, &doubleEncoder3, &doubleEncoder4};
+    // encoders
+    std::array < DoubleEncoder * , 5 > encoders =
+                                           {&doubleEncoder1, &doubleEncoder2,
+                                            &doubleEncoder3, &doubleEncoder4, &doubleEncoder5};
     for (auto enc : encoders) enc->setSensitivity(EncoderSensitivity);
+    encoders[1]->setPressedValue(0.5f); // noteChangeTime
+    encoders[2]->setValue(0.6f); // filter
+    encoders[3]->setValue(0.5f); // envelope attack
+    encoders[4]->setValue(0.5f); // envelope release
 
-    encoders[0]->setPressedValue(0.5f);
-
-    volatile float noteChangeTime = 600.0f;
-    volatile float glideTime = 0.005f;
-    volatile float detune = 0.005f;
-
-    volatile float attackTime = 0.01f;
-    volatile float decayTime = 0.1f;
-    volatile float sustain = 0.2f;
-    volatile float releaseTime = 0.1f;
-
-    volatile float lowpassCutoff = 3000.0f;
-    Envelope &ampEnvelope = monoSynth.getAmplifierEnvelope();
-    LowpassFilter1P &lowpassFilter = monoSynth.getLowpassFilter();
-
+    // synth parameters
+    volatile float glideTime;
+    volatile float detune;
+    volatile float attackTime;
+    volatile float releaseTime;
+    volatile float lowpassCutoff;
 
     // encoder loop
     while (true) {
 
-        size_t noteSize = sizeof(midiNotesValuesSimpleMelody) / sizeof(*midiNotesValuesSimpleMelody);
-        std::vector <Midi::MidiMessage> midiQueue =
-                midiMessagesFromNoteValues(midiNotesValuesSimpleMelody, noteSize);
+        {
+            miosix::FastMutex mutex;
 
-        for (auto midiMsg : midiQueue) {
+            // synth audio modules
+            Envelope &ampEnvelope = monoSynth.getAmplifierEnvelope();
+            LowpassFilter1P &lowpassFilter = monoSynth.getLowpassFilter();
+
             for (auto enc : encoders) enc->update(); // updating the encoder state
+
             // getting values from the encoders
-            noteChangeTime = AudioMath::linearMap(encoders[0]->getPressedValue(), 0.0f, 1.0f, 600.0f, 100.0f);
-            attackTime = encoders[0]->getValue();
-            decayTime = encoders[1]->getValue();
-            releaseTime = encoders[2]->getValue();
-            lowpassCutoff = encoders[3]->getValue();
-            {
-                miosix::FastMutex mutex;
-                ampEnvelope.setAttack(attackTime);
-                ampEnvelope.setDecay(decayTime);
-                ampEnvelope.setSustain(sustain);
-                ampEnvelope.setRelease(releaseTime);
+            noteChangeTime = AudioMath::linearMap(encoders[1]->getPressedValue(), 0.0f, 1.0f, 600.0f, 50.0f);
+            lowpassCutoff = encoders[2]->getValue();
+            attackTime = encoders[3]->getValue();
+            releaseTime = encoders[4]->getValue();
 
-                lowpassFilter.setCutoff(lowpassCutoff);
-
-                monoSynth.processMidi(midiMsg);
-            }
-            miosix::Thread::sleep(noteChangeTime);
+            // setting the synth
+            ampEnvelope.setAttack(attackTime);
+            ampEnvelope.setRelease(releaseTime);
+            lowpassFilter.setCutoff(lowpassCutoff);
         }
+        miosix::Thread::sleep(20);
+
     }
 }
 
+void synthThreadFunc() {
+    // test midi queue
+    size_t noteSize = sizeof(midiNotesValuesSimpleMelody) / sizeof(*midiNotesValuesSimpleMelody);
+    std::vector <Midi::MidiMessage> midiQueue =
+            midiMessagesFromNoteValues(midiNotesValuesSimpleMelody2, noteSize);
+    float noteChangeTimeLocal;
+
+    // synth audio modules
+    {
+        miosix::FastMutex mutex;
+        Envelope &ampEnvelope = monoSynth.getAmplifierEnvelope();
+        LowpassFilter1P &lowpassFilter = monoSynth.getLowpassFilter();
+        ampEnvelope.setSustain(1.0f);
+        ampEnvelope.setDecay(0.2f);
+    }
+
+    while (true) {
+        for (auto midiMsg : midiQueue) {
+            {
+                miosix::FastMutex mutex;
+                monoSynth.processMidi(midiMsg);
+                noteChangeTimeLocal = noteChangeTime;
+            }
+            miosix::Thread::sleep(noteChangeTimeLocal);
+        }
+    }
+}
 
 int main() {
     // initializing the audio driver
@@ -140,9 +154,12 @@ int main() {
 
     // starting the threads
     std::thread hardwareInterfaceThread(hardwareInterfaceThreadFunc);
+    std::thread synthThread(synthThreadFunc);
 
     // starting the audio driver (blocking function)
     audioDriver.start();
+    hardwareInterfaceThread.join();
+    synthThread.join();
 }
 
 #else // testing enabled
