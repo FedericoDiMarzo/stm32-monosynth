558e984b8144c76e5446982c02b32ae69a6eac34
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-04-21 17:09:10 +0200

    Update filter.h
    extracting logMapping method

--- audio_modules/filter.h
+++ audio_modules/filter.h
@@ -30,8 +30,7 @@ public:
     inline void setCutoff(float normalizedValue) {
         normalizedValue = AudioMath::clip(normalizedValue, 0.0f, 1.0f);
         // implements a logarithmic potentiometer with values between 20 and 20k
-        setCutoffFrequency(AudioMath::expLut(
-                AUDIOMATH_LOG20 + normalizedValue * (AUDIOMATH_LOG20000 - AUDIOMATH_LOG20)));
+        setCutoffFrequency(logMapping(normalizedValue));
     };
 
     /**
@@ -47,6 +46,17 @@ protected:
      * Cutoff frequency of the filter in hertz.
      */
     AudioParameter<float> cutoffFrequency;
+
+    /**
+     * Maps logarithmically an input bounded from 0 to 1
+     * to a value between 20 and 20k
+     *
+     * @param normalizedValue value between 0 and 1
+     * @return value between 20 and 20k mapped logarithmically
+     */
+    inline float logMapping(float normalizedValue) {
+        return AudioMath::expLut(AUDIOMATH_LOG20 + normalizedValue * (AUDIOMATH_LOG20000 - AUDIOMATH_LOG20));
+    }
 
 };
 
