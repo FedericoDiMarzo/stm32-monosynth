a9228500583cc58847a2502c090ddc9645ae627d
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-04-22 18:33:49 +0200

    Create synth_configuration.h
    configuration header for the synth

---
+++ synth_configuration.h
@@ -0,0 +1,21 @@
+#ifndef STM32_MONOSYNTH_SYNTH_CONFIGURATION_H
+#define STM32_MONOSYNTH_SYNTH_CONFIGURATION_H
+
+/**
+ * This header is used to change the configuration
+ * for the synthesizer.
+ */
+
+/**
+ * Oscillator algorithm.
+ */
+#define SYNTH_OSCILLATOR_TYPE SAW
+//#define SYNTH_OSCILLATOR_TYPE SINE
+
+/**
+ * Midi in channel.
+ */
+#define SYNTH_MIDI_IN_CH 10
+
+
+#endif //STM32_MONOSYNTH_SYNTH_CONFIGURATION_H
