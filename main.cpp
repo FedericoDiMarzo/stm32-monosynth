8fd5dfd11d8632b0a285784f35a63029f7e44cde
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-04-22 19:31:52 +0200

    Update main.cpp
    midi in

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "../drivers/common/audio.h"
 #include "audio/audio_processor.h"
 #include "drivers/double_encoder.h"
+#include "drivers/midi_in_port.h"
 #include "audio_processors/mono_synth.h"
 #include "audio_modules/envelope.h"
 #include "audio_modules/lowpass_filter_1p.h"
@@ -12,7 +13,9 @@
 #include "tests/hardware_tests/double_encoder_test.h"
 #include "tests/hardware_tests/button_test.h"
 #include "drivers/hardware_configuration.h"
+#include "synth_configuration.h"
 #include <array>
+#include <queue>
 #include <cmath>
 #include <thread>
 
@@ -41,9 +44,6 @@ AudioDriver audioDriver;
 // The synth engine
 MonoSynth monoSynth(audioDriver);
 
-// to change the speed of the sequencer
-volatile float noteChangeTime;
-
 // double encoders
 DoubleEncoder doubleEncoder1(DoubleEncoder1Timer, DoubleEncoder1PortEnc,
                              DoubleEncoder1PinAEnc, DoubleEncoder1PinBEnc,
@@ -61,26 +61,16 @@ DoubleEncoder doubleEncoder5(DoubleEncoder5Timer, DoubleEncoder5PortEnc,
                              DoubleEncoder5PinAEnc, DoubleEncoder5PinBEnc,
                              DoubleEncoder5PortBut, DoubleEncoder5PinBut);
 
-std::vector <Midi::MidiMessage> midiMessagesFromNoteValues(uint8_t *noteArray, size_t size) {
-    std::vector <Midi::MidiMessage> midiQueue;
-    for (size_t i = 0; i < size; i++) {
-        midiQueue.push_back(Midi::MidiMessage({noteArray[i], 100}, Midi::MidiMessageType::NOTE_ON));
-        midiQueue.push_back(Midi::MidiMessage({noteArray[i], 100}, Midi::MidiMessageType::NOTE_OFF));
-    }
-    return midiQueue;
-}
-
 /**
  * Thread function for handling the hardware inputs.
  */
 void hardwareInterfaceThreadFunc() {
     // encoders
-    std::array < DoubleEncoder * , 5 > encoders =
-                                           {&doubleEncoder1, &doubleEncoder2,
-                                            &doubleEncoder3, &doubleEncoder4, &doubleEncoder5};
+    std::array < DoubleEncoder * , 5 > encoders = {&doubleEncoder1, &doubleEncoder2,
+                                                   &doubleEncoder3, &doubleEncoder4, &doubleEncoder5};
     for (auto enc : encoders) enc->setSensitivity(EncoderSensitivity);
     encoders[1]->setValue(0.0f); // glideTime
-    encoders[1]->setPressedValue(0.5f); // noteChangeTime
+//    encoders[1]->setPressedValue(0.5f);
     encoders[2]->setValue(1.0f); // lowpassCutoff
     encoders[2]->setPressedValue(0.0f); // filter cutoff envelope amount
     encoders[3]->setValue(0.0f); // lowpassResonance
@@ -109,9 +99,8 @@ void hardwareInterfaceThreadFunc() {
             for (auto enc : encoders) enc->update(); // updating the encoder state
 
             // getting values from the encoders
-            noteChangeTime = AudioMath::linearMap(encoders[1]->getPressedValue(), 0.0f, 1.0f, 600.0f, 50.0f);
-            glideTime = encoders[1]->getValue();
-//            lowpassCutoff = AudioMath::linearMap(encoders[2]->getValue(), 0.0f, 1.0f, 0.0f, 0.8f);
+//            noteChangeTime = AudioMath::linearMap(encoders[1]->getPressedValue(), 0.0f, 1.0f, 600.0f, 50.0f);
+            glideTime = encoders[1]->getValue() + 0.0001f;
             lowpassCutoff = encoders[2]->getValue();
             filterCutoffModAmt = encoders[2]->getPressedValue();
             lowpassResonance = encoders[3]->getValue();
@@ -119,43 +108,33 @@ void hardwareInterfaceThreadFunc() {
             attackTime = encoders[4]->getPressedValue();
 
             // setting the synth
-//            monoSynth.setGlide(glideTime); // TODO: this parameter can't be updated so fast
+            monoSynth.setGlide(glideTime);
             ampEnvelope.setAttack(attackTime);
             ampEnvelope.setRelease(releaseTime);
             monoSynth.setFilterCutoff(lowpassCutoff);
             monoSynth.setFilterCutoffEnvelopeAmt(filterCutoffModAmt);
             monoSynth.setFilterResonance(lowpassResonance);
         }
-        miosix::Thread::sleep(20);
+        miosix::Thread::sleep(35);
 
     }
 }
 
 void synthThreadFunc() {
-    // test midi queue
-    size_t noteSize = sizeof(midiNotesValuesSimpleMelody4) / sizeof(*midiNotesValuesSimpleMelody4);
-    std::vector <Midi::MidiMessage> midiQueue =
-            midiMessagesFromNoteValues(midiNotesValuesSimpleMelody4, noteSize);
-    float noteChangeTimeLocal;
-
-    // synth audio modules
-    {
-        miosix::FastMutex mutex;
-        Envelope &ampEnvelope = monoSynth.getEnvelope();
-        LowpassFilterLadder4p &lowpassFilter = monoSynth.getLowpassFilter();
-        monoSynth.setGlide(0.005f);
-        ampEnvelope.setSustain(1.0f);
-        ampEnvelope.setDecay(0.2f);
-    }
+    Midi::MidiParser midiParser; // midi parser
+    MidiInPort &midiInPort = MidiInPort::getInstance();
+    auto &midiBuffer = midiParser.getMessageQueue();
 
     while (true) {
-        for (auto midiMsg : midiQueue) {
+        midiInPort.fillTheQueue(1); // thread yields here
+        midiParser.parse(midiInPort.getRawMidiQueue());
+        while (!midiBuffer.empty()) {
+            Midi::MidiMessage tmp = midiBuffer.front();
+            midiBuffer.pop();
             {
                 miosix::FastMutex mutex;
-                monoSynth.processMidi(midiMsg);
-                noteChangeTimeLocal = noteChangeTime;
+                monoSynth.processMidi(tmp);
             }
-            miosix::Thread::sleep(noteChangeTimeLocal);
         }
     }
 }
@@ -165,14 +144,26 @@ int main() {
     audioDriver.init();
     audioDriver.setAudioProcessable(monoSynth);
 
+    // midi configuration
+
+    // synth setup
+    Envelope &ampEnvelope = monoSynth.getEnvelope();
+    LowpassFilterLadder4p &lowpassFilter = monoSynth.getLowpassFilter();
+    monoSynth.setGlide(0.005f);
+    ampEnvelope.setSustain(1.0f);
+    ampEnvelope.setDecay(0.2f);
+
     // starting the threads
     std::thread hardwareInterfaceThread(hardwareInterfaceThreadFunc);
     std::thread synthThread(synthThreadFunc);
 
     // starting the audio driver (blocking function)
     audioDriver.start();
+
+    // join with the thread just for debugging
 //    hardwareInterfaceThread.join();
 //    synthThread.join();
+//    midiThread.join();
 }
 
 #else // testing enabled
